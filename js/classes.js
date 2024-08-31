29876590fa5edf2c5f3a25afa85a20745122834e
Manikanta <prvp6501@gmail.com>
2024-08-31 23:31:39 +0530

    death animation

--- js/classes.js
+++ js/classes.js
@@ -111,7 +111,7 @@ class Fighter extends Sprite {
     this.isAttacking;
     this.health = 100;
     this.sprites = sprites;
-
+    this.dead = false;
     for (const sprite in this.sprites) {
       sprites[sprite].image = new Image();
       sprites[sprite].image.src = sprites[sprite].imageSrc;
@@ -120,7 +120,7 @@ class Fighter extends Sprite {
   //#region update
   update() {
     this.draw();
-    this.animateFrames();
+    if (!this.dead) this.animateFrames();
 
     this.attackBox.position.x = this.position.x - this.attackBox.offset.x;
 
@@ -162,6 +162,16 @@ class Fighter extends Sprite {
   }
   //#region switch sprite
   switchSprite(sprite) {
+    if (
+      //override other animations with death anim
+      this.image === this.sprites.death.image
+    ) {
+      if (this.framesCurrent === this.sprites.death.framesMax - 1) {
+        this.dead = true;
+      }
+      return;
+    }
+
     if (
       //override other animations with attack anim
       // to attack only once
@@ -232,6 +242,15 @@ class Fighter extends Sprite {
         if (this.image !== this.sprites.takeHit.image) {
           this.image = this.sprites.takeHit.image;
           this.framesMax = this.sprites.takeHit.framesMax;
+
+          this.framesCurrent = 0;
+        }
+        break;
+
+      case "death":
+        if (this.image !== this.sprites.death.image) {
+          this.image = this.sprites.death.image;
+          this.framesMax = this.sprites.death.framesMax;
 
           this.framesCurrent = 0;
         }
